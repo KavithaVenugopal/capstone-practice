033f3202ea2cb31bc777b3c8f4a4db74eedbff48
KavithaVenugopal <kavithanov.1611@gmail.com>
2023-09-11 07:00:22 +0000

    script file

---
+++ install.sh
@@ -0,0 +1,22 @@
+sudo apt update
+sudo apt install git -y
+sudo apt install docker.io -y
+sudo usermod -aG docker ubuntu
+sudo apt install openjdk-11-jre -y
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+docker-compose --version
+docker --version
+git --version
+jenkins --version
